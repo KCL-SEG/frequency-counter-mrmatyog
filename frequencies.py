244d76c493c43c55ea164a6df393764309956aa5
Matushan Yogaraj <dev@matushan.com>
2022-10-12 21:46:02 +0100

    Implement function to produce a dictionary based on a list

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,8 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if str(item) not in frequencies:
+            frequencies[str(item)] = 0
+        frequencies[str(item)] += 1
     return frequencies
